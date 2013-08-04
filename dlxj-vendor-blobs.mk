07767439b22f996e8cb03a19bbeea300601e12ac
tako0910 <tako.oct0910@gmail.com>
2013-08-04 18:33:43 +0900

    Remove outdated EGL lib

--- dlxj-vendor-blobs.mk
+++ dlxj-vendor-blobs.mk
@@ -47,7 +47,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/dlxj/proprietary/lib/libcsd-client.so:system/lib/libcsd-client.so \
   vendor/htc/dlxj/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
   vendor/htc/dlxj/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-  vendor/htc/dlxj/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
   vendor/htc/dlxj/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
   vendor/htc/dlxj/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
   vendor/htc/dlxj/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
